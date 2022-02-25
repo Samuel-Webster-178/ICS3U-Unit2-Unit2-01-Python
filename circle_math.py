83be34baa0633017f3086886638ef0503edd3218
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-25 19:34:53 +0000

    innitial commit

---
+++ circle_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Mr. Coxall
+# Created on: Sep 2020
+# This program calculates the area and perimeter of a circle
+# with radius 15mm
+
+import math
+
+
+def main():
+    # I calculate and print the circumference and area
+    print("If a circle has a radius of 10 m: ")
+    print("Area is {} m².".format(math.pi * 15**2))
+    print("Circumference is {} m.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
